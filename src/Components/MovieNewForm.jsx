110a3ac46d9ec70c80a00c28b166421b1635bdfb
Nicole Slater <nicoleslater@pursuit.org>
2023-11-14 19:38:57 -0500

    Updated MovieNewForm.jsx

--- src/Components/MovieNewForm.jsx
+++ src/Components/MovieNewForm.jsx
@@ -1,7 +1,11 @@
-import { useEffect, useState } from "react";
+import { useState } from "react";
+import {useNavigate} from "react-router-dom";
+
+const API = import.meta.env.VITE_BASE_URL
 
 function MovieNewForm(){
-    const [newMovie, setNewMovie] = useState({
+ const navigate = useNavigate();
+ const [movie, setMovie] = useState({
         title: "", 
         url: "",
         released: "", 
@@ -10,8 +14,77 @@ function MovieNewForm(){
         rated: "", 
         is_favorite: false, 
         imdb_Ratings: 0
-    });
-    
+ });
+
+ const addMovie = () => {
+    fetch(`${API}/movies`, {
+        method: "POST",
+        body: JSON.stringify(movie),
+        headers: {
+            "Content-Type": "application/json",
+        },
+    })
+    .then(() => {
+        navigate(`/movies`);
+    })
+    .catch((error) => console.error("catch", error))
+ };
+
+ const handleTextChange = (event) => {
+    setMovie({...movie, [event.target.id]: event.target.value});
+ };
+
+ const handleCheckboxChange = () => {
+    setMovie({...movie, is_favorite: !movie.is_favorite});
+ };
+
+ const handleSubmit = (event) => {
+    event.preventDefault();
+    addMovie();
+ };
+ return (
+    <div className="New">
+        <form onSubmit={handleSubmit}>
+            <label htmlFor="title">Title:</label>
+            <input
+                id="title"
+                value={movie.title}
+                type="text"
+                onChange={handleTextChange}
+                placeholder="Title of the Movie..."
+                required
+                />
+                <label htmlFor="url">URL:</label>
+                <input 
+                id="url"
+                type="text"
+                pattern="http[s]*//.+"
+                required
+                value={movie.url}
+                placeholder="http://"
+                onChange={handleTextChange}
+                />
+                <label htmlFor="genre">Genre:</label>
+                <input 
+                id="genre"
+                type="text"
+                name="genre"
+                value={movie.genre}
+                placeholder="Action, Comedy, ..."
+                onChange={handleTextChange}
+                />
+                <label htmlFor="isFavorite">Favorite:</label>
+                <input
+                id="isFavorite"
+                type="checkbox"
+                onChange={handleCheckboxChange}
+                checked={movie.is_favorite}
+                />
+                <br />
+                <input type="submit" />
+                </form>
+                </div>
+ );
 }
 
 export default MovieNewForm;
